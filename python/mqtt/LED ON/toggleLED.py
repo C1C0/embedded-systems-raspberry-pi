bf39892b91acd4ebda3122cd852bf6a246c424d7
C1C0 <kpskola1002@gmail.com>
2022-03-10 16:01:51 +0100

    LAST EXERCISE

--- python/mqtt/LED ON/toggleLED.py
+++ python/mqtt/LED ON/toggleLED.py
@@ -1,5 +1,40 @@
+from ast import arg
+from cgitb import enable
 import RPi.GPIO as GPIO
 import time
+import paho.mqtt.client as mqtt
+import constants as C
+import threading
+
+class MQTTC:
+    def __init__(self, callback, parameters) -> None:
+        self.client = mqtt.Client(client_id="", userdata=None, protocol=mqtt.MQTTv5)
+        self.__callback = callback
+        self.__params = parameters
+
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.client.username_pw_set(username=C.MQTT_USER, password=C.MQTT_PASS)
+        self.client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+
+    # The callback for when the client receives a connect response from the server.
+    def on_connect(self, client, userdata, flags, rc, idk):
+        print("Connected with result code "+str(rc))
+        print("Flags" + str(flags))
+
+        # on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
+        client.subscribe(C.TOPICS_LED)
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg: mqtt.MQTTMessage):
+        print(msg.topic+" "+str(msg.payload))
+
+        self.__callback(msg.payload.decode(), self.__params)
+
+    def connect(self) -> None:
+        self.client.connect(C.MQTT_SERVER, port=C.MQTT_PORT, keepalive=60)
+        self.client.loop_forever()
 
 class Device:
     def __init__(self, BCMPin) -> None:
@@ -20,6 +55,14 @@ class LED(Device):
         self.__state = not self.__state 
         GPIO.output(self.pin, self.__state)
 
+    def on(self) -> None:
+        self.__state = GPIO.HIGH 
+        GPIO.output(self.pin, self.__state)
+
+    def off(self) -> None:
+        self.__state = GPIO.LOW 
+        GPIO.output(self.pin, self.__state)
+
 class Button(Device):
     def __init__(self, BCMPin, initState = GPIO.LOW, debounceDelay = 60) -> None:
         super().__init__(BCMPin)
@@ -34,7 +77,6 @@ class Button(Device):
 
     def checkPresstimeOfButton(self, callback) -> None:
         actualTime = round(time.time() * 1000)
-        print(self.__lastButtonState)
 
         if actualTime > self.__lastDebounceTimeMS + self.__debounceDelay:
             self.__lastDebounceTimeMS = actualTime
@@ -48,9 +90,21 @@ class Button(Device):
     def getButtonState(self) -> bool:
         return self.__lastButtonState
 
+def checkMqttSwitch(msg, params) -> None:
+    print(msg)
+    if msg == "ON":
+        params[0].on()
+        
+    if msg == "OFF":
+        params[0].off()
+
 def setup() -> None:
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False) 
+
+def enableMQtt(led :LED):
+    mqttClient = MQTTC(checkMqttSwitch, [led])
+    mqttClient.connect()
 
 if __name__ == "__main__":
     setup()
@@ -58,5 +112,9 @@ if __name__ == "__main__":
     btn = Button(15)
     led = LED(2)
 
+    th = threading.Thread(target=enableMQtt, args=(led, ))
+    th.start()
+
     while True:
         btn.checkPresstimeOfButton(led.toggle)
+    
